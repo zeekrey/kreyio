7534e8054839b8aea9cc21eaba871431cff5a865
christian@krey.io <christian@krey.io>
2020-05-08 23:01:33 +0200

    Added gatsby-transformer-remark plugin.
    Blog posts should be written in markdown in the src/blog/ folder. To render the blog post the template under src/templates is used.

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,17 +16,39 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: `gatsby-transformer-remark`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        // CommonMark mode (default: true)
+        // commonmark: true,
+        // Footnotes mode (default: true)
+        // footnotes: true,
+        // Pedantic mode (default: true)
+        // pedantic: true,
+        // GitHub Flavored Markdown mode (default: true)
+        // gfm: true,
+        // Plugins configs
+        // plugins: [],
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `blog`,
+        path: `${__dirname}/src/blog`,
+      },
+    },
+    // {
+    //   resolve: `gatsby-plugin-manifest`,
+    //   options: {
+    //     name: `gatsby-starter-default`,
+    //     short_name: `starter`,
+    //     start_url: `/`,
+    //     background_color: `#663399`,
+    //     theme_color: `#663399`,
+    //     display: `minimal-ui`,
+    //     icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+    //   },
+    // },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
