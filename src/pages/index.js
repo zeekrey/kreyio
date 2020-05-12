5b744d2536a4d573037407029c2098d096189e15
christian@krey.io <christian@krey.io>
2020-05-12 22:56:53 +0200

    Added dark and light mode with styled-components.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,45 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "gatsby"
+import { ThemeProvider } from "styled-components"
+import { lightTheme, darkTheme } from "../styles/theme"
+import { GlobalStyles } from "../styles/globals"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import Toggle from "../components/Toggle"
+import { useDarkMode } from '../components/useDarkMode';
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const IndexPage = () => {
+  // const [theme, setTheme] = useState("light")
+  const [theme, toggleTheme, componentMounted] = useDarkMode();
+  const themeMode = theme === 'light' ? lightTheme : darkTheme;
+
+  if (!componentMounted) {
+    return <div />
+  };
+  
+  // The function that toggles between themes
+  // const toggleTheme = () => {
+  //   // if the theme is not light, then set it to dark
+  //   if (theme === "light") {
+  //     setTheme("dark")
+  //     // otherwise, it should be light
+  //   } else {
+  //     setTheme("light")
+  //   }
+  // }
+
+  return (
+    <ThemeProvider theme={themeMode}>
+      <>
+        <GlobalStyles />
+        <Toggle theme={theme} toggleTheme={toggleTheme} />
+        <h1>It's a {theme === 'light' ? 'light theme' : 'dark theme'}!</h1>
+        <footer></footer>
+      </>
+    </ThemeProvider>
+  )
+}
 
 export default IndexPage
