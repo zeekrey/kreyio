72581112d624a0357be1cbce8073dd020bd2071e
christian@krey.io <christian@krey.io>
2020-05-17 22:34:43 +0200

    :shirt: Removed linter warning.

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,51 +1,8 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React, { useState } from "react"
+import React from "react"
 import styled from "styled-components"
+import { Link } from "gatsby"
 import { ThumbsDown, ThumbsUp } from "react-feather"
 import SocialIcons from "./socialIcons"
-
-const Header = styled.header`
-  padding-top: ${({ theme }) => theme.paddingY};
-  padding-bottom: ${({ theme }) => theme.paddingY};
-  padding-left: ${({ theme }) => theme.paddingX};
-  padding-right: ${({ theme }) => theme.paddingX};
-  display: flex;
-  justify-content: space-between;
-  flex-direction: row;
-  position: relative;
-`
-
-const Brand = styled.div`
-  flex: 1;
-  color: ${({ theme }) => theme.secondary};
-`
-
-const Nav = styled.div`
-  flex: 1;
-  text-align: end;
-`
-
-const Menu = styled.div`
-  padding-top: ${({ theme }) => theme.paddingY};
-  padding-bottom: ${({ theme }) => theme.paddingY};
-  padding-left: ${({ theme }) => theme.paddingX};
-  padding-right: ${({ theme }) => theme.paddingX};
-  box-shadow: 0px 0px 1px rgba(0, 0, 0, 0.04), 0px 0px 2px rgba(0, 0, 0, 0.06),
-    0px 4px 8px rgba(0, 0, 0, 0.04);
-`
-
-const ThemeMenu = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  column-gap: 1.2rem;
-`
-
-const MeOn = styled.div`
-  text-align: center;
-  margin-top: 1.4rem;
-  color: ${({ theme }) => theme.secondary};
-`
 
 const Wrapper = styled.div`
   background-color: #f4f4f4;
@@ -107,13 +64,5 @@ const Footer = () => {
     </Wrapper>
   )
 }
-
-// Header.propTypes = {
-//   siteTitle: PropTypes.string,
-// }
-
-// Header.defaultProps = {
-//   siteTitle: ``,
-// }
 
 export default Footer
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,7 @@
-import React, { useState } from "react"
-import ThemeButton from "../components/themeButton"
+import React from "react"
 import Body from "../components/body"
-import styled from "styled-components"
 import Navigation from "../components/navigation"
-import Footer from "../components/Footer"
-
-const Container = styled.div`
-  width: 100%;
-  background-color: green;
-`
+import Footer from "../components/footer"
 
 const IndexPage = () => {
   return (
